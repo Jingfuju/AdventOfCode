3c9678c596c984e63a9406ac2a82c607a35de6ce
Jingfu Ju <jingfuju@Jeffs-Mac-M2.local>
2024-12-29 13:28:17 -0800

    initial

---
+++ 2024/2024/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  2024
+//
+//  Created by Jingfu Ju on 12/29/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
